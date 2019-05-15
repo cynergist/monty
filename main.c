a12353a27f5ca7d8710f3149465a9aff09fd7cf4
Cyn <cmdominguez@gmail.com>
2019-05-15 23:12:57 +0000

    add main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * main - Reads opcodes from a file and implements them
+ *
+ *@argv: String of arguments
+ *@argc: Number of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+/*	int o;
+	char *buffer; */
+	(void)argv;
+
+	if (argc < 2)
+		printf("USAGE: monty file\n");
+	return (EXIT_FAILURE);
+}
