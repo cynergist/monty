ceae8d451b447ca2bc718222a5cef155541a2630
Amber <abmcbride5@gmail.com>
2019-05-16 01:00:22 +0000

    updated main file to accept a file from the command line and read it's contents into a buffer. also included some of the proper error handling messages

--- main.c
+++ main.c
@@ -9,11 +9,40 @@
  */
 int main(int argc, char *argv[])
 {
-/*	int o;
-	char *buffer; */
-	(void)argv;
+	int o, r;
+	char *buffer; 
 
 	if (argc < 2)
+	{
 		printf("USAGE: monty file\n");
-	return (EXIT_FAILURE);
+		return (EXIT_FAILURE);
+	}
+	o = open(argv[1], O_RDONLY);
+	/* having a hard time testing this*/
+	if (o == -1)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	buffer = malloc(sizeof(char)* 100);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (EXIT_FAILURE);
+	}
+	r = read(o, buffer, 100);
+	if (r == -1)
+	{
+		printf("Error: Unable to read file %s", argv[1]);
+		return(EXIT_FAILURE);
+	}
+	printf("%s", buffer);
+	/*the print statment above was just a test to see if the 
+	file was being opened and the contents properly stored in 
+	the buffer. it was successful.
+	the code to parse the contents of buffer and then
+	feed that newly parsed string into the getfunc will go here.
+	that will be the end of this main file*/
+	free(buffer);
+	return (EXIT_SUCCESS);
 }
