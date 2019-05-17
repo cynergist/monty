0e26161f4e20ed44c6e8aa645e6c8a43ce0196dd
Amber <abmcbride5@gmail.com>
2019-05-17 05:18:18 +0000

    betty pass for opcode functions

--- opcode_functions.c
+++ opcode_functions.c
@@ -32,25 +32,27 @@ void op_pall(stack_t **head, __attribute__((unused)) unsigned int n)
 {
 	print_dlistint(*head);
 }
+/**
+*op_pint - function to print the first node added
+*
+*@head: point to the beginning of the function
+*@n: line number
+*
+*Return: void
+*/
 void op_pint(stack_t **head, unsigned int n)
 {
 	stack_t *temp;
-	
- 	temp= get_dnodeint_at_index(*head, 0);
-	if(temp)
+
+	temp = get_dnodeint_at_index(*head, 0);
+	if (temp)
 	{
 		printf("%d\n", temp->n);
 	}
 	else
 	{
-		printf("L%d: can't pint, stack empty", n);
+		printf("L%d: can't pint, stack empty\n", n);
 		clean(1);
 		exit(EXIT_FAILURE);
 	}
 }
-/*void op_pop(stack_t **head, unsigned int n)
-{
-		
-}*/
-
-
